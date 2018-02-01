7925db1a449c82b638f94c425ce45f09b1923284
Chengpeng Wang <chpengwang@gmail.com>
2018-02-02 02:19:47 +0800

    validate Job title - must exist

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,6 +1,7 @@
 class Job < ApplicationRecord
   validates :wage_upper_bound, numericality: { greater_than: :wage_lower_bound }
   validates :wage_lower_bound, numericality: { greater_than: 0.0 }
+  validates :title, presence: true
   validates :contact, presence: true
 
   scope :recent, -> {order("updated_at DESC")}
