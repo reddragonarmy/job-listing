b05da7284e4995f3893e339863743e0f8e959302
Chengpeng Wang <chpengwang@gmail.com>
2018-02-02 03:34:21 +0800

    move require_is_admin to root class so that it can be call everywhere

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def require_is_admin
+    unless current_user.is_admin?
+      redirect_to root_path, alert: "你没有权限"
+    end
+  end
 end
