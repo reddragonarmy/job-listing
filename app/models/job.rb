1c79aed3b610fab58fa14fb5337b29c860805b81
Chengpeng Wang <chpengwang@gmail.com>
2018-02-02 01:47:55 +0800

    add validation to job - wage must exist and be numeric, contact must exist

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,4 @@
 class Job < ApplicationRecord
+  validates :wage_upper_bound, numericality: { greater_than: :wage_lower_bound }
+  validates :wage_lower_bound, numericality: { greater_than: 0.0 }
 end
